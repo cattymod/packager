c35ddf879b0d8f283e7941f1b0a2089a36a9654c
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-09-01 14:56:06 +0100

    Update brand.js

--- src/packager/brand.js
+++ src/packager/brand.js
@@ -42,6 +42,7 @@ https://www.mozilla.org/en-US/MPL/2.0/.`,
   FEEDBACK_PRIMARY: {
     name: 'Scratch',
     link: 'https://scratch.mit.edu/projects/1335032585/',
+  },
   DONATE: '',
   PRIVACY_POLICY: 'https://studio.cattymod.app/privacy.html',
 };
